1ba1d3369d21894e428032729800352ef399be7c
Emmanuel ASIFIWE <emmanuel2koestner@gmail.com>
2021-01-27 23:10:19 +0200

    updated underground mining coding part

--- cmms-innovation.ino
+++ cmms-innovation.ino
@@ -16,8 +16,8 @@ DHT dht(DHTPIN, DHTTYPE);
 MQ2 mq2(MQGASPIN);
 
 // Replace with WIFI network credentials
-const char* ssid = "junior";
-const char* password = "junior121212";
+const char* ssid = "CANALBOX-B53A";
+const char* password = "6383905460";
 
 // HTTP status
 HTTPClient http;
@@ -51,6 +51,12 @@ String buzzerState;
 // url to make a request
 String url = "https://cmms-app.herokuapp.com/monitoring/update-data?data=";
 
+float tempThreshold = 32.00;
+float humiThreshold = 100.00;
+float lpgThreshold = 1000.00;
+float coThreshold = 70000.00;
+float smokeThreshold = 5200.00;
+
 //required function to collecting data
 // temperature sensing function that return value of temperature
 String readDHTTemperature() {
@@ -65,6 +71,7 @@ String readDHTTemperature() {
     return "--";
   }
   else {
+    Serial.print("Temperature: ");
     Serial.println(t);
     return String(t);
   }
@@ -79,6 +86,7 @@ String readDHTHumidity() {
     return "--";
   }
   else {
+    Serial.print("Humidity: ");
     Serial.println(h);
     return String(h);
   }
@@ -92,6 +100,7 @@ String readDHTTemperatureF() {
     return "--";
   }
   else {
+    Serial.print("Fahrenheit: ");
     Serial.println(f);
     return String(f);
   }
@@ -106,6 +115,7 @@ String readMQLPG() {
     return "--";
   }
   else {
+    Serial.print("LPG: ");
     Serial.println(lpg);
     return String(lpg);
   }
@@ -120,6 +130,7 @@ String readMQCo() {
     return "--";
   }
   else {
+    Serial.print("CO: ");
     Serial.println(co);
     return String(co);
   }
@@ -133,6 +144,7 @@ String readMQSmoke() {
     return "--";
   }
   else {
+    Serial.print("Smoke: ");
     Serial.println(smoke);
     return String(smoke);
   }
@@ -171,32 +183,29 @@ void loop()
   String coValue = readMQCo();
   String smokeValue = readMQSmoke();
   String tempFValue = readDHTTemperatureF();
+  Serial.println(" ");
+
   // give access to the client to Async webserver.
   WiFiClient client = server.available();
 
-  if (client) {                             // If a new client connects,
+  if (client) {
     currentTime = millis();
     previousTime = currentTime;
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+    Serial.println("New Client.");
+    String currentLine = "";
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {
       currentTime = millis();
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
         header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
+        if (c == '\n') {
           if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
             client.println("HTTP/1.1 200 OK");
             client.println("Content-type:text/html");
             client.println("Connection: close");
             client.println();
 
-            // turns the GPIOs on and off
             if (header.indexOf("GET /26/on") >= 0) {
               Serial.println("GPIO 26 on");
               output26State = "on";
@@ -212,7 +221,6 @@ void loop()
             client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
             client.println("<link rel=\"icon\" href=\"data:,\">");
             // CSS to style the on/off buttons
-            // Feel free to change the background-color and font-size attributes to fit your preferences
             client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
             client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
             client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
@@ -220,8 +228,7 @@ void loop()
 
             // Web Page Heading
             client.println("<body><h1>CMMS Web Server</h1>");
-
-            // Emmanuel try
+            client.println("<body><h1>CMMS Data Collected</h1>");
             client.println("<p>Temperature Value: " + tempValue + "</p>");
             client.println("<p>Humidity Value: " + humiValue + "</p>");
             client.println("<p>LPG Value: " + lpgValue + "</p>");
@@ -229,9 +236,8 @@ void loop()
             client.println("<p>Smoke Value: " + smokeValue + "</p>");
             client.println("<p>Fahrenheit Value: " + tempFValue + "</p>");
 
-            // Display current state, and ON/OFF buttons for GPIO 26
+            // Give Access to the buzzer
             client.println("<p>GPIO 26 - State " + output26State + "</p>");
-            // If the output26State is off, it displays the ON button
             if (output26State == "off") {
               client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
             } else {
@@ -244,15 +250,14 @@ void loop()
             client.println();
             // Break out of the while loop
             break;
-          } else { // if you got a newline, then clear currentLine
+          } else {
             currentLine = "";
           }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
+        } else if (c != '\r') {
+          currentLine += c;
         }
       }
     }
-    // Clear the header variable
     header = "";
     // Close the connection
     client.stop();
@@ -267,6 +272,14 @@ void loop()
   co = readMQCo();
   smoke = readMQSmoke();
   delay(1000);
+
+  if ((temperature.toFloat() > tempThreshold) || (humidity.toFloat() > humiThreshold) || (lpg.toFloat() > lpgThreshold) || (co.toFloat() > coThreshold) || (smoke.toFloat() > smokeThreshold)) {
+    output26State = "on";
+    digitalWrite(output26, HIGH);
+  } else {
+    output26State = "off";
+    digitalWrite(output26, LOW);
+  }
 
   // reconnect just in case
   while (WiFi.status() != WL_CONNECTED) {
